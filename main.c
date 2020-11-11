9b8a3134e93d2a184a3a22a70230bf22c0ddf6be
SheLesTT <74094256+SheLesTT@users.noreply.github.com>
2020-11-11 19:55:34 +0300

    fitrst commit

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define mod(y) ((y)<0)? -(y):(y)
+
+int main()
+{
+
+
+    double p,p1, S, x, n, E ;
+
+    n =1;
+    while (n!=1000)
+    {
+        printf("Enter  x, n and E \n");
+        scanf("%lf %lf %lf", &x, &n, &E);
+        p = 1;
+
+        for(int k=1; k <= n; k++)
+        {
+            p = p * (x/2);
+        }
+
+        S = p;
+
+        for (int i = 1; i <= n; i++)
+        {
+
+            p1 = p;
+            p =  p *(x/2)*(x/2)/i;
+
+            S = S +p;
+
+        }
+
+
+        printf( " SUM =  %1.15lf \n",S);
+
+    }
+    return 0;
+}
